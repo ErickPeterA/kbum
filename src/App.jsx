8c7270c74ccd356e779627d55c1214a936e2541b
ErickPeterA <erickpeterdearaujo@gmail.com>
2025-11-03 22:06:36 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import ItemList from './components/ItemList'
+import ItemDetail from './components/ItemDetail'
+import ItemForm from './components/ItemForm'
+
+export default function App(){
+  return (
+    <div className="container">
+      <header className="header">
+        <div>
+          <div className="logo"><img src="logo.png" alt="" /></div>
+        </div>
+        <nav>
+          <Link to="/" className="small" style={{marginRight:12}}>Home</Link>
+          <Link to="/novo" className="small">Cadastrar Item</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route  path="/novo" element={<ItemForm />} />
+        <Route path="/itens/:id" element={<ItemDetail />} />
+      </Routes>
+    </div>
+  )
+}
